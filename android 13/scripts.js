c07790c8fa088224201b45e0b0b53799c0efb42b
Kacper <kacperkawec481@gmail.com>
2026-02-28 16:51:25 +0000

    fix

--- android 13/scripts.js
+++ android 13/scripts.js
@@ -1,8 +1,8 @@
-let hourr = 0
+let hourr = 9
 function clockgo(){
-    document.getElementById("hi").style.transform = "rotate(45deg)";
-    hourr = hourr + 30;
-    if (hourr === 120){
+    document.getElementById("hi").style.transform = "rotate(30deg)";
+    hourr = hourr + 1;
+    if (hourr === 13){
 document.getElementById("htmlbodywow").style.backgroundColor = "lightblue"
 document.writeln("ANDROID 13")
     }
