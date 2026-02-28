58893d10730d0e7267067479791ce8d5cb761adf
Kacper <kacperkawec481@gmail.com>
2026-02-28 19:09:08 +0000

    add more styles

--- android 13/scripts.js
+++ android 13/scripts.js
@@ -1,9 +1,9 @@
 let hourr = 0
 function clockgo(){
         hourr = hourr + 15;
-        document.getElementById("hi").style.transform = `rotate(${hourr}deg)`;
+        document.getElementById("hi").style.transform = `rotation(${hourr})`;
     if (hourr === 90){
-document.getElementById("htmlbodywow").style.backgroundColor = "lightblue"
+document.getElementById("htmlbodywow").style.backgroundColor = "skyblue"
     }
 }
 //use backquote thingies for the ${yourvariable here} thing to work ig cuz it just does
