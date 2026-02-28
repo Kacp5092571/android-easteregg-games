d45c859d155325ba08eb4dfa5675adf767f6832c
Kacper <kacperkawec481@gmail.com>
2026-02-28 16:26:24 +0000

    new things

---
+++ android 13/scripts.js
@@ -0,0 +1,9 @@
+let hourr = 0
+function clockgo(){
+    document.getElementById("hi").style.transform = "rotate(45deg)";
+    hourr = hourr + 30;
+    if (hourr === 120){
+document.getElementById("htmlbodywow").style.backgroundColor = "lightblue"
+document.writeln("ANDROID 13")
+    }
+}
