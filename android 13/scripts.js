8da220bdaa13ac1dce471fe45f48649be0e048c3
Kacper <kacperkawec481@gmail.com>
2026-02-28 17:01:59 +0000

    a

--- android 13/scripts.js
+++ android 13/scripts.js
@@ -1,6 +1,6 @@
 let hourr = 9
 function clockgo(){
-    document.getElementById("hi").style.transform = "rotate(30deg)";
+    document.getElementById("hi").style.transform = "rotate(15deg)";
     hourr = hourr + 1;
     if (hourr === 13){
 document.getElementById("htmlbodywow").style.backgroundColor = "lightblue"
