b28279cac8e87c71a1d6a83ed4e41e655ff6d2e1
Kacper <kacperkawec481@gmail.com>
2026-02-28 17:29:37 +0000

    fix number values and stuff

--- android 13/scripts.js
+++ android 13/scripts.js
@@ -2,8 +2,8 @@ let hourr = 0
 function clockgo(){
         hourr = hourr + 15;
         document.getElementById("hi").style.transform = `rotate(${hourr}deg)`;
-    if (hourr === 60){
+    if (hourr === 90){
 document.getElementById("htmlbodywow").style.backgroundColor = "lightblue"
     }
 }
-//use backquote thingies for the ${yourvariable here} thing to work
+//use backquote thingies for the ${yourvariable here} thing to work ig cuz it just does
